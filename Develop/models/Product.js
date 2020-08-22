0ad66204b49718a26b9b9a08adccec19f01d5072
Desert Python <mattsenneff@aol.com>
2020-08-21 20:38:54 -0700

    models update with new references

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -33,7 +33,10 @@ Product.init(
     },
     category_id:{
       type: DataTypes.INTEGER,
-      //references the category model id
+      references: {
+        model: 'category',
+        key: 'id',
+      }
     }
 
   },
